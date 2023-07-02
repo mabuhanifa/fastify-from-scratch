711e932376bbca1cae6c09e20ed3794c8e9a1c8d
shourovahmd <moh.abuhanifa@gmail.com>
2023-07-03 05:29:16 +0600

    routes

--- server.js
+++ server.js
@@ -2,10 +2,19 @@ const fastify = require("fastify");
 const app = fastify();
 const port = process.env.PORT || 3000;
 
-app.listen({ port }, (err, address) => {
+app.listen({ port, host: "0.0.0.0" }, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
   }
   console.log(`Server listening at ${address}`);
+});
+
+app.get("/", async (request, reply) => {
+  reply.send({ hello: "world" });
+});
+
+app.get("/:id", async (request, reply) => {
+  const { id } = request.params;
+  reply.send({ hello: "world", id: id });
 });
