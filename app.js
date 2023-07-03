bd56c1f88c87246b4ed59c38f018a67acebad59b
shourovahmd <moh.abuhanifa@gmail.com>
2023-07-03 06:08:01 +0600

    app cleared

--- app.js
+++ app.js
@@ -1,28 +1,15 @@
-'use strict'
+const fastify = require("fastify");
+const app = fastify();
+const port = process.env.PORT || 3000;
 
-const path = require('path')
-const AutoLoad = require('@fastify/autoload')
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
-// Pass --options via CLI arguments in command to enable these options.
-module.exports.options = {}
-
-module.exports = async function (fastify, opts) {
-  // Place here your custom code!
-
-  // Do not touch the following lines
-
-  // This loads all plugins defined in plugins
-  // those should be support plugins that are reused
-  // through your application
-  fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'plugins'),
-    options: Object.assign({}, opts)
-  })
-
-  // This loads all plugins defined in routes
-  // define your routes in one of these
-  fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'routes'),
-    options: Object.assign({}, opts)
-  })
-}
+app.listen({ port }, (err, address) => {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+  console.log(`Server is running on ${address}`);
+});
