fd0357e4d6645b1ab22e6030f314522a51e48537
shourovahmd <moh.abuhanifa@gmail.com>
2023-07-03 06:48:44 +0600

    data fetched

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const fastify = require("fastify");
 const app = fastify();
 const port = process.env.PORT || 3000;
+const mysql = require("@fastify/mysql");
+require("dotenv").config();
+
+app.register(mysql, {
+  connectionString: process.env.DATABASE_URL,
+});
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  app.mysql.query("SELECT * FROM users", (err, data) => {
+    if (err) {
+      res.send({
+        status: "error",
+        error,
+      });
+    }
+    res.send({ success: true, data: data });
+  });
 });
 
 app.listen({ port }, (err, address) => {
@@ -11,5 +25,6 @@ app.listen({ port }, (err, address) => {
     console.log(err);
     process.exit(1);
   }
+
   console.log(`Server is running on ${address}`);
 });
